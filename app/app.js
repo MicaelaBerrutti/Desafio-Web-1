14b0c31247c7b1fa0e9baf2a706acb7443c0186b
MicaelaBerrutti <micarobe10@gmail.com>
2023-07-31 19:36:23 -0300

    Se realizan correcciones

--- app/app.js
+++ app/app.js
@@ -1,24 +1,23 @@
-//elementos html
 let body = document.body;
+
 let opcionesMenu = ["Home", "Equipo", "Servicios", "Contacto"];
 var contenedorMenu = document.createElement("div");
-opcionesMenu.forEach(opcion => {
-var texto = document.createElement("a");
-texto.href = "";
-contenedorMenu.appendChild(texto); 
-texto.textContent = opcion; 
-    
+
+opcionesMenu.forEach(opcion => {          //por cada elemento del array opcionesMenu, se crea y se agrega un link en el contenedorMenu
+    var texto = document.createElement("a");
+    texto.href = "";
+    contenedorMenu.appendChild(texto); 
+    texto.textContent = opcion; 
 });
+
 contenedorMenu.classList.add("menu"); 
 
-let activado = false;
+let activado = false; //activado = se muestra el menú
 
 function desplegarMenu(){
     if(!activado){
         body.appendChild(contenedorMenu);
-        //contenedorMenu.appendChild(icono);
-        
-    activado = true;
+        activado = true;
     }else{
         body.removeChild(contenedorMenu);
         activado=false;
